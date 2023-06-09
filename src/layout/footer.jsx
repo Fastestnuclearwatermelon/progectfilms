3144d9e37f82c45923e81447e7f3ede0c342395e
Данил Панов <denpanov150@gmail.com>
2023-06-10 10:23:30 +1200

    Сделать проект

---
+++ src/layout/footer.jsx
@@ -0,0 +1,12 @@
+function Footer(){
+    return <footer className="page-footer black">
+
+          <div className="footer-copyright">
+            <div className="container">
+            © 2023 Danil Panov
+            <a class="grey-text text-lighten-4 right" href="#!">Repository</a>
+            </div>
+          </div>
+    </footer>
+}
+export { Footer}; 
